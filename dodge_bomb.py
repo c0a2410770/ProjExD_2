ee63d8e580a12502afd247a73f9b7d8a83137eb2
Masaya Sato <c0a2410770@edu.teu.ac.jp>
2025-09-30 14:11:28 +0900

    練習2:爆弾作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -20,6 +21,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20,20))  # 練習2:爆弾作成
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
+    vx, vy = +5, +5  # 爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
     
@@ -29,13 +38,14 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        for key, mv in DELTA.items(): #練習1
+        for key, mv in DELTA.items():  # 練習1
             if key_lst[key]:
-                sum_mv[0] += mv[0] #横方向の移動量を加算
-                sum_mv[1] += mv[1] #縦方向の移動量を加算
+                sum_mv[0] += mv[0]  # 横方向の移動量を加算
+                sum_mv[1] += mv[1]  # 縦方向の移動量を加算
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -46,6 +56,8 @@ def main():
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  #爆弾移動
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
